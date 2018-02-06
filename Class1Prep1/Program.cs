b8d3553694396206d30135c0069143f2b57ecbad
Daniel <danielbrogers594@gmail.com>
2018-02-06 13:01:37 -0500

    first commit

---
+++ Class1Prep1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Class1Prep1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name=Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+            Console.ReadLine();
+        }
+    }
+}
